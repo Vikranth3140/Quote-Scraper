01549cdd3dd7806bf30bc83e213006cc860562e4
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-04-23 19:18:26 +0530

    Dumped quotes to scraped_quotes/page_name

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 
@@ -6,19 +7,26 @@ base_url = 'http://quotes.toscrape.com'
 # Initial page URL
 url = base_url + '/page/1'
 
-# Function to scrape quotes and authors from a page
-def scrape_page(url):
+# Create a directory to store scraped data
+if not os.path.exists('scraped_quotes'):
+    os.makedirs('scraped_quotes')
+
+# Function to scrape quotes and authors from a page and save to a text file
+def scrape_page(url, page_num):
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'lxml')
         quote_blocks = soup.find_all('div', class_='quote')
-        for quote_block in quote_blocks:
-            quote = quote_block.find('span', class_='text').text.strip()
-            author = quote_block.find('small', class_='author').text.strip()
-            print(f'Quote: {quote}\nAuthor: {author}\n')
+        file_name = f'scraped_quotes/page_{page_num}.txt'
+        with open(file_name, 'w', encoding='utf-8') as file:
+            for quote_block in quote_blocks:
+                quote = quote_block.find('span', class_='text').text.strip()
+                author = quote_block.find('small', class_='author').text.strip()
+                file.write(f'Quote: {quote}\nAuthor: {author}\n\n')
 
 # Scraping the first page
-scrape_page(url)
+print(f'Scraping page 1...')
+scrape_page(url, 1)
 
 # Scraping subsequent pages if available
 page_num = 2
@@ -27,7 +35,7 @@ while True:
     response = requests.get(next_page)
     if response.status_code == 200:
         print(f'Scraping page {page_num}...')
-        scrape_page(next_page)
+        scrape_page(next_page, page_num)
         page_num += 1
     else:
         break
